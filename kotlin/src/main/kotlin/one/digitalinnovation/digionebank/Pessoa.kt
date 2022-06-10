ae2f13523c67927bef756505230416bcb640bc3c
douglas <faladouglas@live.com>
2022-06-10 11:33:20 -0300

    update to make person and employee  abstract class

--- kotlin/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ kotlin/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,22 +1,8 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
+abstract class Pessoa(
+    val nome:String,
+    val cpf: String,
+    ) {
 
-    var nome : String = "douglas"
-        private set //não permite modificar os dados de outros lugares !!! porem permite usar o get (chamada)
-    var cpf:String = "367.585.896-98"
-        private set
-
-constructor()
-    fun pessoainfo() = "$nome \n$cpf"
-
-
-
-}
-fun main(){
-    val douglas = Pessoa()
-
-    //println(douglas.nome)
-    //println(douglas.cpf)
-    println(douglas.pessoainfo())
 }
