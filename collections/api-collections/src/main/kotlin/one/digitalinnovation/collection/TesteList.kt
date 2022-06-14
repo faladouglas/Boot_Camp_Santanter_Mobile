18e0e5cb7e8568eec6ede35c77bfae5396394158
douglas <faladouglas@live.com>
2022-06-14 14:26:49 -0300

    using sorteby in

--- collections/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ collections/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val douglas = funcionario("douglas",9500.0)
-    val pedro = funcionario("pedro",2000.0)
-    val matheus = funcionario("matheus",5500.0)
+    val douglas = funcionario("douglas",9500.0, 31)
+    val pedro = funcionario("pedro",2000.0,20)
+    val matheus = funcionario("matheus",5500.0,25)
 
     val funcionarios = listOf(douglas, pedro , matheus)
     funcionarios.forEach{
@@ -13,21 +13,25 @@ fun main() {
     }
     println("---------------------------")
     println("existe um funcionario de nome douglas ? \n"+ funcionarios.any { it.nome == "douglas" })
-    println("\n"+ funcionarios.find { it.nome == "douglas" })
 
-
+    println("\nordem por salario\n")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+    println("\nordem por idade\n")
+    funcionarios.sortedBy { it.idade }.forEach{ println(it) }
 
 }
 
 data class funcionario(
     val nome:String,
     val salario:Double,
+    val idade:Int,
 
 
 ){
     override fun toString(): String = """
         Nome: $nome
         Salario: $salario
+        Idade: $idade
     """.trimIndent()
 
     }
