9249aaf11b747e6648725abd2ed1bfb66f620fb5
douglas <faladouglas@live.com>
2022-06-14 14:45:31 -0300

    using groupby in listof

--- collections/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ collections/api-collections/src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val douglas = funcionario("douglas",9500.0, 31)
-    val pedro = funcionario("pedro",2000.0,20)
-    val matheus = funcionario("matheus",5500.0,25)
+    val douglas = funcionario("douglas",9500.0, 31,"clt")
+    val pedro = funcionario("pedro",2000.0,20,"autonomo")
+    val matheus = funcionario("matheus",5500.0,25,"autonomo")
 
     val funcionarios = listOf(douglas, pedro , matheus)
     funcionarios.forEach{
@@ -15,9 +15,10 @@ fun main() {
     println("existe um funcionario de nome douglas ? \n"+ funcionarios.any { it.nome == "douglas" })
 
     println("\nordem por salario\n")
-    funcionarios.sortedBy { it.salario }.forEach { println(it) }
-    println("\nordem por idade\n")
-    funcionarios.sortedBy { it.idade }.forEach{ println(it) }
+    funcionarios.sortedBy { it.salario }.forEach { println(it) } // organiza de forma crescente
+
+    println("\nordenação por tipo de  contrato\n")
+    funcionarios.groupBy { it.tipoContratacao }.forEach{ println(it) } //groupby faz agrupamentos de elementos
 
 }
 
@@ -25,6 +26,7 @@ data class funcionario(
     val nome:String,
     val salario:Double,
     val idade:Int,
+    val tipoContratacao:String,
 
 
 ){
@@ -32,6 +34,7 @@ data class funcionario(
         Nome: $nome
         Salario: $salario
         Idade: $idade
+        Contrato: $tipoContratacao
     """.trimIndent()
 
     }
